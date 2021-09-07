69332a4ce3566274317929a6ac8ab8c8d2559ec8
Harshvardhan <harshvardhan@cse.iitb.ac.in>
2021-09-07 07:06:10 +0530

    explore and update added

--- githubProfiles/ProfileRepo/urls.py
+++ githubProfiles/ProfileRepo/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('profile/<int:user_id>/', views.profile, name='profile'),
-    path('profile/', views.profile, name='profile'),
+    path('profile/update/<int:user_id>/', views.update, name='update'),
+    path('profile/explore/', views.explore, name='explore')
 ]
--- githubProfiles/ProfileRepo/views.py
+++ githubProfiles/ProfileRepo/views.py
@@ -1,14 +1,49 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import redirect,render
 from .models import Profile, Repository
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+import requests
+from django.utils import timezone
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
+REQUESTS_BASE_DIR = 'https://api.github.com/users/'
 
 def profile(request, user_id):
-    user = User.objects.get(id=user_id)
-    return render(request, 'profile.html', {'user':user})
+    user = request.user
+    if user.is_authenticated:
+        user1 = User.objects.get(id=user_id)
+        return render(request, 'profile.html', {'user':user1, 'current_user_id':user.id,})
+    else:
+        return redirect('/accounts/login')
+    
+
+def explore(request):
+    user = request.user
+    if user.is_authenticated:
+        return render(request, 'explore.html', {
+            'user_list':User.objects.all(), 
+            'current_user_id':user.id,
+        })
+    else:
+        return redirect('/accounts/login')
+
+def update(request, user_id):
+    user = request.user
+    if user.is_authenticated:
+        user = User.objects.get(id=user_id)
+        response = requests.get(REQUESTS_BASE_DIR+user.username)
+        data = response.json()
+        user.profile.last_update_time = timezone.localtime() 
+        user.profile.followers = data['followers']
+        user.profile.save()
+
+        user.profile.repository_set.all().delete()
+        response = requests.get(REQUESTS_BASE_DIR+user.username+'/repos')
+        repos_data = response.json()
+        for repo in repos_data:
+            Repository.objects.create(profile=user.profile, name=repo["name"], num_stars=repo["stargazers_count"])
+        return redirect('/profile/'+str(user.id))
+    else:
+        return redirect('/accounts/login')
+
 
 
