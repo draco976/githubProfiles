929c9d04139ea34ec61425ff96970b86bc5a20dc
Harshvardhan <harshvardhan@cse.iitb.ac.in>
2021-09-10 17:11:31 +0530

    adding style.css

--- githubProfiles/settings.py
+++ githubProfiles/settings.py
@@ -22,11 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
+# SECURITY WARNING: keep the secret key used in production secret!
+if DEBUG==False:
+    SECRET_KEY = os.environ['SECRET_KEY']
+else:
+    SECRET_KEY = config('SECRET_KEY')
 
 ALLOWED_HOSTS =['*'] 
 
@@ -129,6 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    str(BASE_DIR.joinpath('static'))
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
