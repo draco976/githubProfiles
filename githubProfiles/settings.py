bf45ed3a2f70a01e0423762acb888710f6cd0136
Harshvardhan <harshvardhan@cse.iitb.ac.in>
2021-09-11 23:37:29 +0530

    adding whitenoise for static files

--- githubProfiles/settings.py
+++ githubProfiles/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 ROOT_URLCONF = 'githubProfiles.urls'
